960ffc3a082347311e8e96a041fb6d676e0c248e
Ameejac <ameejacherukuru123@gmail.com>
2021-11-10 18:37:46 +0530

    ameeja [added:checking file is Exist are Not]

---
+++ FileIOoperations/FileReadWriteOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Linq;
+
+namespace FileIOoperations.FileIO
+{
+    class FileReadWriteOperation
+    {
+       
+        public   void  FileExist( string Path)
+        {
+            
+            if (File.Exists(Path))
+            {
+                Console.WriteLine("file is Exist");
+                
+            }
+            else
+            {
+                Console.WriteLine("file is not Exist");
+                
+            }
+
+
+        }
+    }
+}
---
+++ FileIOoperations/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FileIOoperations.FileIO;
+namespace FileIOoperations.FileIO
+{
+    class Program
+    {
+         static void Main(string[] args)
+         {
+
+            string Path = @"C:\Users\Dell\Desktop\AmeejaFolder\FileIOoperations\FileIOoperations\Sample.txt";
+            FileReadWriteOperation file = new FileReadWriteOperation();
+            file.FileExist(Path);
+            Console.WriteLine("");
+          }
+    }
+}
